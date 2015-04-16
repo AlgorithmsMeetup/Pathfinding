bf4bf548bf47d5907dba1dcdee5f1b02464948e4
Mike Luby <Michael@mLuby.com>
2015-04-15 22:57:53 -0700

    pull in old repo assets (visualizer)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Pathfinding
-Dijkstra's Algorithm, A*
+# Pathfinding with Dijkstra's Algorithm & A*
+
+![Djikstra](https://upload.wikimedia.org/wikipedia/commons/5/57/Dijkstra_Animation.gif)
+
+![A*](https://upload.wikimedia.org/wikipedia/commons/8/85/Weighted_A_star_with_eps_5.gif)
---
+++ pathfinder.js
@@ -0,0 +1,20 @@
+function djikstra(node2, node1, graph){
+  // Fill in this function
+
+
+  return 'shortest distance list of nodes from node1 to node2';
+};
+
+function heuristic(?){
+  // Fill in this function
+
+
+  return '?';
+};
+
+function astar(node2, node1, graph){
+  // Fill in this function
+
+
+  return 'shortest distance list of nodes from node1 to node2';
+};
