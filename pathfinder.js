565e98ada97297bf3629e164b55caa6093d92eec
Mike Luby <Michael@mLuby.com>
2015-04-18 01:24:09 -0700

    add node/graph

--- pathfinder.js
+++ pathfinder.js
@@ -1,42 +1,37 @@
-function djikstra(node2, node1){
+function djikstra(node1, node2){
   // Fill in this function
-
-
   return []; //'shortest distance list of nodes from node1 to node2';
 };
 
-function heuristic(?){
+function heuristic(idk){
   // Fill in this function
-
-
   return '?';
 };
 
-function astar(node2, node1){
+function astar(node1, node2){
   // Fill in this function
-
-
   return []; //'shortest distance list of nodes from node1 to node2';
 };
 
 // Graph
 function Node(cost){
-  this.wasVisited = false;
+  this.cost = cost;
+  this.visited = false;
   this.neighbors = [];
 };
 
-Node.prototype.visit = function() {
-  this.wasVisited = true;
+Node.prototype.visit = function(value) {
+  this.visited = value || true;
 };
 
-Node.prototype.connectToNode = function(node){
+Node.prototype.connectTo = function(node){
   if(this.neighbors.indexOf(node) === -1){
     this.neighbors.push(node);
+    node.connectTo(this);
   }
-  node.connectToNode(this);
 };
 
 // test methods; don't use these!
 Node.prototype.unvisit = function() {
-  this.wasVisited = false;
+  this.visited = false;
 };
