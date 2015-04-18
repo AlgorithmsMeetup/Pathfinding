ae8c51dae7f03f0339988ba7c3ead225a7fc46ef
Mike Luby <Michael@mLuby.com>
2015-04-17 23:57:58 -0700

    WIP implmenting graph nodes

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-# Pathfinding with Dijkstra's Algorithm & A*
+# Pathfinding: Dijkstra's Algorithm & A*
 
+##Djikstra's Algorithm ("dike-straw")
 ![Djikstra](https://upload.wikimedia.org/wikipedia/commons/5/57/Dijkstra_Animation.gif)
 
+##A* ("a-star")
 ![A*](https://upload.wikimedia.org/wikipedia/commons/8/85/Weighted_A_star_with_eps_5.gif)
--- pathfinder.js
+++ pathfinder.js
@@ -1,8 +1,8 @@
-function djikstra(node2, node1, graph){
+function djikstra(node2, node1){
   // Fill in this function
 
 
-  return 'shortest distance list of nodes from node1 to node2';
+  return []; //'shortest distance list of nodes from node1 to node2';
 };
 
 function heuristic(?){
@@ -12,9 +12,31 @@ function heuristic(?){
   return '?';
 };
 
-function astar(node2, node1, graph){
+function astar(node2, node1){
   // Fill in this function
 
 
-  return 'shortest distance list of nodes from node1 to node2';
+  return []; //'shortest distance list of nodes from node1 to node2';
+};
+
+// Graph
+function Node(cost){
+  this.wasVisited = false;
+  this.neighbors = [];
+};
+
+Node.prototype.visit = function() {
+  this.wasVisited = true;
+};
+
+Node.prototype.connectToNode = function(node){
+  if(this.neighbors.indexOf(node) === -1){
+    this.neighbors.push(node);
+  }
+  node.connectToNode(this);
+};
+
+// test methods; don't use these!
+Node.prototype.unvisit = function() {
+  this.wasVisited = false;
 };
